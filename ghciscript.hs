176553984246c9caec202f55bbef0f0133f012db
Philip Lykke Carlsen <plcplc@gmail.com>
2013-01-30 15:51:22 +0100

    first alpha of unfold on push arrays. not quite sure what to think about it..

---
+++ ghciscript.hs
@@ -0,0 +1,8 @@
+:set +m
+-- This small script supports interactive handling of nikola experiments .. overwrite at your leisure.
+:module +Data.Array.Nikola.Backend.CUDA.Haskell
+:module +Data.Array.Nikola.Backend.Main
+:module +Data.Array.Nikola.Playground
+:module +Data.Array.Nikola.Repr.Push
+
+:set args "--cuda" "-V" "10" "-o" "nikola.out"
